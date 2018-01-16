63c0c236d6ab038d2899208cba8f5848af916234
andresum97 <andresum1309@gmail.com>
2018-01-16 17:39:52 -0600

    Se agrega la clase interfaz, con todos los métodos a utilizar por la radio.

---
+++ HojaTrabajo1/src/douglas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andres Urizar
+ * @author David Soto
+ */
+public interface douglas {
+    
+    public void onOff();  //Método para prender/apagar la radio
+    
+    public void Switch(); //Método para cambiar AM/FM
+    
+    public float siguiente(float a);  //Método para cambiar a la siguiente estación
+    
+    public float anterior(float a);  //Método para cambiar a la estación anterior
+    
+    public void guardar(float e,int b);  //Método para guardar una estación en favoritos
+    
+    public float selecccionarFav(int b);  //Método para devolver una de las estación que estén en favoritos
+            
+}
